43ddcb7a612df2360e53a231be74eae86f352e20
Jean Holguin <holguinjean1@gmail.com>
2026-08-05 19:15:08 -0400

    Appsettings production template Adds logging on calculation requests.

--- SubjectPlanner.Api/Controllers/SubjectsController.cs
+++ SubjectPlanner.Api/Controllers/SubjectsController.cs
@@ -1,4 +1,6 @@
+using System.Text.Json;
 using Microsoft.AspNetCore.Mvc;
+using Serilog;
 using SubjectPlanner.Core;
 using SubjectPlanner.Core.Helpers;
 using SubjectPlanner.Core.Services;
@@ -10,19 +12,27 @@ namespace SubjectPlanner.Api.Controllers
     public class SubjectsController : ControllerBase
     {
         private readonly SubjectService _subjectService;
+        private readonly ILogger<SubjectsController> _logger;
 
-        public SubjectsController(SubjectService subjectService)
+        public SubjectsController(SubjectService subjectService, ILogger<SubjectsController> logger)
         {
             _subjectService = subjectService;
+            _logger = logger;
         }
 
         [HttpPost]
         public IActionResult GetEndDate(Subject subjectDto)
         {
             try {
+                string requestJson = JsonSerializer.Serialize(subjectDto);
+                _logger.LogInformation(requestJson);
+
                 if (subjectDto.Hours < 0) return BadRequest(new CalculationResult{ ClassDays = 0, EndDate = subjectDto.StartDate, Holidays = []});
 
                 CalculationResult calculation = _subjectService.Calculate(subjectDto ?? new Subject());
+
+                string calculationString = JsonSerializer.Serialize(calculation);
+                _logger.LogInformation(message: calculationString);
 
                 return Ok(new {
                     Ok = true,
